5c2f5cde9b81f905992bcdacace2ac4b28bc7cc0
Samuelsimwata <simwatasamuel@gmail.com>
2022-09-08 13:48:49 -0700

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,7 +12,7 @@
 
 * this program prints "Programming is like building a multilingual puzzle
 
-* Return: 0
+* Return (0);
 
 */
 
@@ -20,7 +20,7 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
